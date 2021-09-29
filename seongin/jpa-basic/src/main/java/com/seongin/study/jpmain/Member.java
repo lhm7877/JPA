672addd955c8779709d700c7a751ed110e45b0a2
ksi0921 <ksi0921@midasit.com>
2021-09-29 19:29:55 +0900

    feat: 실습 예제1 추가 및 내용 정리

--- seongin/jpa-basic/src/main/java/com/seongin/study/jpmain/Member.java
+++ seongin/jpa-basic/src/main/java/com/seongin/study/jpmain/Member.java
@@ -1,22 +1,56 @@
 package com.seongin.study.jpmain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(unique = true, length = 10)
-	private String name;
-	private int age;
+	@Column(name = "name", nullable = false)
+	private String username;
 
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	private LocalDate testLocalDate;
+
+	private LocalDateTime testLocalDateTime;
+
+	@Lob
+	private String description;
+	//Getter, Setter…
+
+	@Transient
+	private int temp;
 	public Member() {}
 
 	public Member(Long id, String name) {
 		this.id = id;
-		this.name = name;
+		this.username = username;
 	}
 
 	public Long getId() {
@@ -28,11 +62,11 @@ public class Member {
 	}
 
 	public String getName() {
-		return name;
+		return username;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		this.username = name;
 	}
 }
 
