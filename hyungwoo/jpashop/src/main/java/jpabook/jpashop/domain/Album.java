a2c7f4dc71dcf32e4e53cd624ecea8f2bdfb79ed
Wu22e <gulb1602@gmail.com>
2021-10-19 22:51:01 +0900

    feat : 상속관계 매핑 실전예제 코드

---
+++ hyungwoo/jpashop/src/main/java/jpabook/jpashop/domain/Album.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Album extends Item{
+
+    private String artist;
+    private String etc;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
