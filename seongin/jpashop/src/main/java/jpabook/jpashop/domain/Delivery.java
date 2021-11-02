7a9addac1e465e8267ebf6be04a806b50c9f57b5
ksi0921 <ksi0921@midasit.com>
2021-11-02 22:15:16 +0900

    feat: 실전예제 6

--- seongin/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ seongin/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import static javax.persistence.FetchType.*;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,13 +15,43 @@ public class Delivery extends BaseEntity{
 	@Id
 	@GeneratedValue
 	private Long id;
-
-	private String city;
-	private String street;
-	private String zipcode;
-
 	private  DeliveryStatus deliveryStatus;
+
+	@Embedded
+	private Address address;
 
 	@OneToOne(mappedBy = "delivery", fetch = LAZY)
 	private Order order;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DeliveryStatus getDeliveryStatus() {
+		return deliveryStatus;
+	}
+
+	public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 }
--- seongin/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ seongin/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,10 +15,9 @@ public class Member extends BaseEntity {
 	@Column(name = "MEMBER_ID")
 	private Long id;
 	private String name;
-	private String city;
-	private String street;
-	private String zipcode;
 
+	@Embedded
+	private Address address;
 	// 의미가 없다. Order를 찾으려면 Order로 가라!
 	//@OneToMany(mappedBy = "member")
 	//private List<Order> orderList = new ArrayList<>();
@@ -38,27 +38,11 @@ public class Member extends BaseEntity {
 		this.name = name;
 	}
 
-	public String getCity() {
-		return city;
+	public Address getAddress() {
+		return address;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 }
