cc13ecc749733c5de5189e2154e35856557b848b
Hyungwoo Kim <52458039+Wu22e@users.noreply.github.com>
2021-11-17 12:31:57 +0900

    section 10, 11 실습

--- hyungwoo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hyungwoo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -21,34 +24,13 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자"); // HashSet에 들어감
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER", Member.class).getResultList();
 
-            System.out.println("============= START =============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity"); // 값 타입을 setter로 변경하면 안된다!
-//            Address old = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", old.getStreet(), old.getZipcode())); // 이렇게 완전히 새로운 인스턴스로 갈아 끼워야 함
-//
-//            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000")); // 여기서 equals, hashCode가 구현이 안되있으면 제대로 동작하지 않는다.
-//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+            for (Member member1: resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
